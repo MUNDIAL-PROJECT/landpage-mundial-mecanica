453e98422f7e355287048b8d40484a36ffe736a2
Guilherme Gomes <gguife747@gmail.com>
2023-08-03 22:47:21 -0300

    Component Hero e Faleconosco criado

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,19 @@
+import { BackgroundImage, DarkOverlay, HeroContainer, HeroContent } from './styled';
+import { Link } from 'react-router-dom';
+export const Hero = () => {
+  
+  return (
+    <HeroContainer>
+      <HeroContent>
+        <h1>Mecânica Mundial</h1>
+        <p>“Confiabilidade e Performance em Cada Rotação, <br /> somos a Mecânica de Confiança!”</p>
+        <div className="hero-buttons">
+          <Link to="/sobre">Sobre nós</Link>
+          <a href="#faleconosco">Contato</a>
+        </div>
+      </HeroContent>
+      <BackgroundImage />
+      <DarkOverlay />
+    </HeroContainer>
+  )
+}
