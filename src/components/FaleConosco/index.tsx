d11db14b7a13dadb738666c5e6bade8eb62d7f37
Guilherme Gomes <gguife747@gmail.com>
2023-08-04 23:04:27 -0300

    Estilizando componente FaleConosco

--- src/components/FaleConosco/index.tsx
+++ src/components/FaleConosco/index.tsx
@@ -1,13 +1,16 @@
 import { FaleContainer, FaleContentForm, FaleContentImg } from './styled';
+import Maps from '../../assets/mapsimage.png';
 
 export const FaleConosco = () => {
   return (
     <FaleContainer id="faleconosco">
       <FaleContentImg>
-        Imagem map google
+        <img src={Maps} alt="localização"  />
+        <span> Rua Dr. Tuany Toledo Júnior, 20 | Bairro Nova Pouso Alegre <br /> CEP 37553-476</span>
       </FaleContentImg>
       <FaleContentForm>
-        <form action="">
+        <form action="" className='faleForm'>
+          <h3>Entre em contato conosco</h3>
           <input type="text" placeholder='Nome' required />
           <input type="email" placeholder='email' required />
           <textarea name="textarea" id="textarea" placeholder='Escreva sua mensagem'></textarea>
--- src/components/FaleConosco/styled.ts
+++ src/components/FaleConosco/styled.ts
@@ -1,25 +1,77 @@
 import { styled } from "styled-components";
 
 export const FaleContainer = styled.section`
-  height: 50vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  padding: 20px 20px;
+  gap: 1rem;
+
+  
+  @media(max-width: 800px){
+    flex-direction: column;
+  }
 `;
 
 export const FaleContentImg = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  span{
+    margin-top: 3px;
+    text-align: center;
+    font-size: .6rem;
+    color: #555;
+  }
+
+  img{
+    width: 90%;
+    height: 100%;
+    border-radius: 20px;
+  }
   
+  @media(max-width: 800px){
+    order: 2;
+  }
 `;
 
 export const FaleContentForm = styled.div`
-  
+  width: 100%;
 
-  form{
+  .faleForm{
     display: flex;
     flex-direction: column;
-    
+    gap: 1rem;
+    margin: 0 auto;
+    max-width: 600px;
+
+    input{
+      padding: 10px 10px;
+      border-radius: 10px;
+      border: none;
+
+    }
     textarea{
+      padding: 10px 10px;
+      border-radius: 10px;
+      border: none;
       resize: none;
+    }
+    button{
+      background-color: ${(props) => props.theme["mundial-gold"]};
+      padding: 15px;
+      border-radius: 5px;
+      border: none;
+      color: #FFF;
+      font-weight: 700;
+      cursor: pointer;
+      transition: .4s;
+  
+      &:hover{
+        opacity: .6;
+      }
     }
   }
 `;
